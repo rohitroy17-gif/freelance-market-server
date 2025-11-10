3ddcd7fef1d9c77bdef10b873c63766a385d7425
rohitroy17-gif <roy172758@gmail.com>
2025-11-10 10:53:41 +0600

    server

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const cors = require('cors');
+const app = express()
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.send('freelance server is running')
+})
+
+app.listen(port, () => {
+  console.log(`freelance server is running on port: ${port}`)
+})
