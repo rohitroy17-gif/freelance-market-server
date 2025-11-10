5ceb7bcc7bdb1a8428417cffb2ee97e90f121357
rohitroy17-gif <roy172758@gmail.com>
2025-11-10 11:12:30 +0600

    products

--- index.js
+++ index.js
@@ -1,15 +1,49 @@
 const express = require('express')
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json())
+const uri = "mongodb+srv://freelance:Rohit@50521@cluster0.4yiiw4x.mongodb.net/?appName=Cluster0";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
 
 app.get('/', (req, res) => {
   res.send('freelance server is running')
 })
 
+async function run() {
+  try {
+    await client.connect();
+
+    const db = client.db('freelance_db');
+    const productsCollection = db.collection('products');
+
+    app.post('/products',async(req,res)=>{
+        const newProduct=req.body;
+        const result= await productsCollection.insertOne(newProduct)
+        res.send(result);
+    })
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    
+  }
+}
+run().catch(console.dir);
+
+
 app.listen(port, () => {
-  console.log(`freelance server is running on port: ${port}`)
+  console.log(`freelance server is running now on port: ${port}`)
 })
